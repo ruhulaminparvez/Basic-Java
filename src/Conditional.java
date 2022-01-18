cd6dc4437415a12a99de4806f7b7c48868e7d5cf
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-01-18 22:06:35 +0600

    Update

--- src/Conditional.java
+++ src/Conditional.java
@@ -1,13 +1,69 @@
 package src;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Conditional {
     public static void main(String[] args) {
         int a = 5;
+        int b = 7;
 
 //        a = a + 2;
+//
+//        a--;
+//        System.out.println(a);
 
-        a--;
-        System.out.println(a);
+//        boolean answer = a == b;
+//        System.out.println(answer);
+
+//        boolean answer = a==5 && b==7;
+//        System.out.println(answer);
+
+//        if (a>3){
+//            System.out.println("A is greater than 3");
+//        } else if (a<2){
+//
+//        }else{
+//
+//        }
+//
+//        System.out.println("Continue.....");
+
+//
+
+//        for (int i=0; i<a; i++){
+//            System.out.println("Hello");
+//
+//        }
+
+//        while (a<10){
+//            System.out.println("Hello");
+//            a++;
+//        }
+
+//        do {
+//            System.out.println("Hello");
+//            a++;
+//        }while (a<10);
+
+
+
+//        System.out.println("Please Enter a Number");
+//        Scanner scanner = new Scanner(System.in);
+//        int input = scanner.nextInt();
+//        System.out.println("Answer Was: " + input);
+//
+//
+//        String str = scanner.next();
+//        System.out.println("Answer Was: " + str);
+
+        Random random = new Random();
+        int ranInt = random.nextInt();
+        System.out.println("Number is: " + ranInt);
+
+
+
+
 
     }
 }
